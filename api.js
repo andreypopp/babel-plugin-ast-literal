63534d7a4842cb9b6d9627ba61d7d1b261e325e1
Andrey Popp <8mayday@gmail.com>
2016-06-02 13:56:48 +0300

    feat: add /api module

---
+++ api.js
@@ -0,0 +1,9 @@
+module.exports.expr = function() {
+  throw new Error('babel-plugin-ast-literal is not active');
+};
+
+module.exports.stmt = function() {
+  throw new Error('babel-plugin-ast-literal is not active');
+};
+
+
